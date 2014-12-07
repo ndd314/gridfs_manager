96f0bfb4b08d1247d3c54bbd2c36c18feb313610
Duy Nguyen <ndd314@gmail.com>
2014-12-06 23:21:44 -0800

    User factory uses Faker::Internet.password

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :user do
     email { Faker::Internet.safe_email }
-    password "Password123$"
-    password_confirmation "Password123$"
+    password { Faker::Internet.password(8) }
   end
 end
