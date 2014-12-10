fcdd61be0e2de8e1c24ee29a7baf24202b06c78d
Duy Nguyen <ndd314@gmail.com>
2014-12-10 11:56:16 -0800

    Return error messages when adding or deleting file

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -13,16 +13,23 @@ class FilesController < ApplicationController
     uploaded_io = params[:gridfs_file][:file_contents]
     folder_id = params[:gridfs_file][:folder_id]
 
-    @gridfs_file = GridfsFile.new(folder_id: folder_id, name: uploaded_io.original_filename)
-    @gridfs_file.upload!(uploaded_io)
-    @gridfs_file.save!
-
-    redirect_to folder_path(id: folder_id)
+    begin
+      @gridfs_file = GridfsFile.new(folder_id: folder_id, name: uploaded_io.original_filename)
+      @gridfs_file.upload!(uploaded_io)
+      @gridfs_file.save!
+      redirect_to folder_path(id: folder_id), notice: 'File uploaded successfully.'
+    rescue StandardError => e
+      redirect_to folder_path(id: folder_id), alert: e.message
+    end
   end
 
   def destroy
-    @gridfs_file = GridfsFile.find(params[:id])
-    @gridfs_file.destroy
-    redirect_to folder_path(@gridfs_file.folder)
+    begin
+      @gridfs_file = GridfsFile.find(params[:id])
+      @gridfs_file.destroy
+      redirect_to folder_path(@gridfs_file.folder)
+    rescue StandardError => e
+      redirect_to folder_path(@gridfs_file.folder), alert: e.message
+    end
   end
 end
